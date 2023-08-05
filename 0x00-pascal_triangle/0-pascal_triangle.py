f6cae04ef011637ac6f92f650deb2fbbc783400c
root <root@Yusuf.localdomain>
2023-08-05 12:35:15 +0100

    added

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,10 +10,10 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         prev_row = triangle[i - 1]
-        
+
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
-        
+
         row.append(1)
         triangle.append(row)
 
