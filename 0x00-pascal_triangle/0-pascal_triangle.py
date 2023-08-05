fe8d661e8b2d78fe6b5eb7277b1adc580b83de2a
root <root@Yusuf.localdomain>
2023-08-05 12:32:50 +0100

    added

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module contains a pascals triangle function """
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+        
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
