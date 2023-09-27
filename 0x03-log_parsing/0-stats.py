eeea1d89b938db9d1a98121760643e66a0e84ec8
whalay <soliuy5@gmail.com>
2023-09-27 18:02:38 +0100

    added

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python3
-"""Log parsing"""
+"""
+    script that reads stdin line by line and computes metrics
+"""
 import sys
-import re
 
 
-def print_stats(size, status_codes):
-    """ prints the statistics """
-
-    print("File size: {}".format(size))
-    for key in sorted(status_codes.keys()):
-        if status_codes[key] != 0:
-            print("{}: {}".format(key, status_codes[key]))
+def print_msg(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
-pattern = re.compile(
-    r"([0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9])\ \-\ \[([0-9]{4}\-[0-1]{1}[0-9]{1}\-[0-3]{1}[0-9]{1}\ [0-2]{1}[0-9]{1}\:[0-5]{1}[0-9]{1}\:[0-5]{1}[0-9]{1}\.[0-9]{6})\]\ (\"GET\ \/projects\/260\ HTTP\/1\.1\")\ (200|301|400|401|403|404|405|500)\ ([1-9][0-9]*)")
-
-size = 0
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
+file_size = 0
+code = 0
+count_lines = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
 }
-counter = 0
 
 try:
     for line in sys.stdin:
-        if re.match(pattern, line) is None:
-            continue
-        line = line.strip()
-        line = line.split()
-        size += int(line[-1])
-        try:
-            status_codes[line[-2]] += 1
-        except KeyError:
-            pass
-        counter += 1
-        if counter == 10:
-            print_stats(size, status_codes)
-            counter = 0
-except KeyboardInterrupt:
-    print_stats(size, status_codes)
-    raise
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
+
+        if len(parsed_line) > 2:
+            count_lines += 1
+
+            if count_lines <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
+
+                if (code in codes.keys()):
+                    codes[code] += 1
+
+            if (count_lines == 10):
+                print_msg(codes, file_size)
+                count_lines = 0
+
+finally:
+    print_msg(codes, file_size)
